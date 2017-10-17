0f0b013d8a0e90e13898cd94f1341250eca37489
mehul0810 <mehul.gohil0810@gmail.com>
2017-10-17 15:08:05 +0530

    Added class for donation_form_grid shortcode

---
+++ includes/admin/shortcodes/shortcode-give-donation-form-grid.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The [donation_form_gri] ShortCode Generator class
+ *
+ * @package     Give
+ * @subpackage  Admin
+ * @copyright   Copyright (c) 2016, WordImpress
+ * @license     https://opensource.org/licenses/gpl-license GNU Public License
+ * @since       1.8.15
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Give_Shortcode_Donation_Form_Grid extends Give_Shortcode_Generator {
+
+	/**
+	 * Class constructor
+	 */
+	public function __construct() {
+
+		$this->shortcode['label'] = __( 'Donation Form Grid', 'give' );
+
+		parent::__construct( 'donation_form_grid' );
+	}
+}
+
+new Give_Shortcode_Donation_Form_Grid;
